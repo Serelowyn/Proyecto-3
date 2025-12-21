02f71fc6c195a85d369a3e5076920d0140edfc40
Memo <fretn655@gmail.com>
2025-12-20 22:53:33 -0600

    ya elimine los datos innecesarios para poder leer los archivos csv

--- Proyecto_sprint_4.py
+++ Proyecto_sprint_4.py
@@ -5,11 +5,11 @@ from matplotlib import pyplot as plt
 
 # ------------------------------------- FIN DE LAS IMPORTACIONES -----------------------------------
 
-df_orders = pd.read_csv("/datasets/instacart_orders.csv")
-df_products = pd.read_csv("/datasets/products.csv")
-df_aisles = pd.read_csv("/datasets/aisles.csv")
-df_departments = pd.read_csv("/datasets/departments.csv")
-df_orderproducts = pd.read_csv("/datasets/order_products.csv")
+df_orders = pd.read_csv("instacart_orders.csv")
+df_products = pd.read_csv("products.csv")
+df_aisles = pd.read_csv("aisles.csv")
+df_departments = pd.read_csv("departments.csv")
+df_orderproducts = pd.read_csv("order_products.csv")
 
 # mostrar información del DataFrame
 print(df_orders)
