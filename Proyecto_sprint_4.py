0d02e6606b9ad2a59f68a9ac61a1c144b3cd8466
Memo <fretn655@gmail.com>
2025-12-20 23:03:07 -0600

    hice la modificacion de la separacion con punto y coma, ya que me aparecia de forma incorrecta por el archivo csv

--- Proyecto_sprint_4.py
+++ Proyecto_sprint_4.py
@@ -1,15 +1,15 @@
-# ---------------------------------------- IMPORTACIONES ------------------------------------------
+# ---------------------------------------- IMPORTACIONES -------------------------------------
 
 import pandas as pd
 from matplotlib import pyplot as plt
 
-# ------------------------------------- FIN DE LAS IMPORTACIONES -----------------------------------
+# ------------------------------------- FIN DE LAS IMPORTACIONES -----------------------------
 
-df_orders = pd.read_csv("instacart_orders.csv")
-df_products = pd.read_csv("products.csv")
-df_aisles = pd.read_csv("aisles.csv")
-df_departments = pd.read_csv("departments.csv")
-df_orderproducts = pd.read_csv("order_products.csv")
+df_orders = pd.read_csv("instacart_orders.csv", sep=";")
+df_products = pd.read_csv("products.csv", sep=";")
+df_aisles = pd.read_csv("aisles.csv", sep=";")
+df_departments = pd.read_csv("departments.csv", sep=";")
+df_orderproducts = pd.read_csv("order_products.csv", sep=";")
 
 # mostrar información del DataFrame
 print(df_orders)
