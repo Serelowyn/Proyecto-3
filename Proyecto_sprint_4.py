588b5f485ec3eb15d6c250e85a4cd31ad68a9bdf
Memo <fretn655@gmail.com>
2025-12-20 22:30:07 -0600

    se agregan importaciones

---
+++ Proyecto_sprint_4.py
@@ -0,0 +1,6 @@
+# ---------------------------------------- IMPORTACIONES ------------------------------------------
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# ------------------------------------- FIN DE LAS IMPORTACIONES -----------------------------------
