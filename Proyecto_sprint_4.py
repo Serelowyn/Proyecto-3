0d65cd2ae3cb17b5baaf76ae4742de86e78209ee
Memo <fretn655@gmail.com>
2025-12-20 22:31:09 -0600

    se agregan los archivos csv, no los tengo yo en mi poder, son exclusivos para la plataforma de triple TEN

--- Proyecto_sprint_4.py
+++ Proyecto_sprint_4.py
@@ -4,3 +4,9 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 # ------------------------------------- FIN DE LAS IMPORTACIONES -----------------------------------
+
+df_orders = pd.read_csv("/datasets/instacart_orders.csv")
+df_products = pd.read_csv("/datasets/products.csv")
+df_aisles = pd.read_csv("/datasets/aisles.csv")
+df_departments = pd.read_csv("/datasets/departments.csv")
+df_orderproducts = pd.read_csv("/datasets/order_products.csv")
