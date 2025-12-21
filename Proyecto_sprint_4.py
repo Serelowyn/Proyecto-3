17d2945a2cb78844d6341d73c29a02407d964d00
Memo <fretn655@gmail.com>
2025-12-20 22:32:03 -0600

    se imprimen los data frames

--- Proyecto_sprint_4.py
+++ Proyecto_sprint_4.py
@@ -10,3 +10,10 @@ df_products = pd.read_csv("/datasets/products.csv")
 df_aisles = pd.read_csv("/datasets/aisles.csv")
 df_departments = pd.read_csv("/datasets/departments.csv")
 df_orderproducts = pd.read_csv("/datasets/order_products.csv")
+
+# mostrar información del DataFrame
+print(df_orders)
+print(df_products)
+print(df_aisles)
+print(df_departments)
+print(df_orderproducts)
